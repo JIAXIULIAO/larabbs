37cddf93b31b52dda2df5e51270d9e660b1ee96a
vagrant <vagrant@homestead>
2021-01-25 02:17:15 +0000

    基础页面

---
+++ blog/resources/views/static_pages/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Weibo App</title>
+</head>
+<body>
+
+        @extends('layouts.default')
+        @section('title', '关于')
+        @section('content')
+
+@stop
+  <h1>关于页</h1>
+</body>
+</html>
