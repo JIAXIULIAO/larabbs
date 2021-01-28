3f75b15442be7262f241fc5dd489014db439a4e9
vagrant <vagrant@homestead>
2021-01-28 09:42:07 +0000

    创建会话

--- blog/app/Http/Controllers/SessionsController.php
+++ blog/app/Http/Controllers/SessionsController.php
@@ -7,18 +7,6 @@ use Auth;
 
 class SessionsController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('guest', [
-            'only' => ['create']
-        ]);
-
-        // 限流 10 分钟十次
-        $this->middleware('throttle:10,10', [
-            'only' => ['store']
-        ]);
-    }
-
     public function create()
     {
         return view('sessions.create');
@@ -31,26 +19,12 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentials, $request->has('remember'))) {
-            if(Auth::user()->activated) {
-               session()->flash('success', '欢迎回来！');
-               $fallback = route('users.show', Auth::user());
-               return redirect()->intended($fallback);
-           } else {
-               Auth::logout();
-               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
-               return redirect('/');
-           }
+       if (Auth::attempt($credentials)) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
        } else {
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back()->withInput();
        }
-    }
-
-    public function destroy()
-    {
-        Auth::logout();
-        session()->flash('success', '您已成功退出！');
-        return redirect('login');
     }
 }
